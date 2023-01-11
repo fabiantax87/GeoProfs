d045fdf6786e1fc35f6bea01b73c8e7cc44730de
fabiantax87 <fabiantax87@users.noreply.github.com>
2023-01-11 10:24:33 +0100

    added base werknemer model

---
+++ GeoProfs/Models/Werknemer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeoProfs.Models
+{
+    public class Werknemer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+    }
+}
