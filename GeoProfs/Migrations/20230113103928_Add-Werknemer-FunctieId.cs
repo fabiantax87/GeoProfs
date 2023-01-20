0cd0078757b1360e94c844f5833a35ba9285b447
fabiantax87 <74959529+fabiantax87@users.noreply.github.com>
2023-01-20 10:46:24 +0100

    Als gebruiker wil ik de website als een bepaalde rol kunnen bekijken (#5) (#6)
    * added base werknemer model
    
    * added simple login
    
    Co-authored-by: fabiantax87 <fabiantax87@users.noreply.github.com>
    
    Co-authored-by: fabiantax87 <fabiantax87@users.noreply.github.com>

---
+++ GeoProfs/Migrations/20230113103928_Add-Werknemer-FunctieId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeoProfs.Migrations
+{
+    public partial class AddWerknemerFunctieId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Functie_id",
+                table: "Werknemers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Functie_id",
+                table: "Werknemers");
+        }
+    }
+}
